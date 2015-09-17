e94532f725fa6b97fe86b2567a75a46344bd2421
davidamin <amin.david.m@gmail.com>
2015-09-17 13:41:08 -0400

    Line Endings and definition order

--- h1/first/models.py
+++ h1/first/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Vehicle(models.Model):
+    max_seats = models.IntegerField(blank=True)
+    trunk_space = models.FloatField(blank=True)
+    notes = models.CharField(max_length = 500, blank=True)
+    condition = models.CharField(max_length = 100, blank=True)
+    make = models.CharField(max_length = 30, blank=True)
+    model = models.CharField(max_length = 30, blank=True)
+    year = models.IntegerField(blank=True)
+    color = models.CharField(max_length = 15, blank=True)
+    plates = models.CharField(max_length = 10, blank=True)
+    uninsured = models.BooleanField(default=False)
+    road_assistance = models.CharField(max_length = 30, blank=True)
+    accomodations = models.CharField(max_length = 30, blank=True)
+
 class User(models.Model):
     #username = models.CharField(max_length=24, unique=True)
     first = models.CharField(max_length=20)
@@ -18,20 +32,6 @@ class User(models.Model):
     #ratings???
     vehicle = models.ForeignKey(Vehicle)
     active = models.BooleanField(default=True)
-	
-class Vehicle(models.Model):
-    max_seats = models.IntegerField(blank=True)
-    trunk_space = models.FloatField(blank=True)
-    notes = models.CharField(max_length = 500, blank=True)
-    condition = models.CharField(max_length = 100, blank=True)
-    make = models.CharField(max_length = 30, blank=True)
-    model = models.CharField(max_length = 30, blank=True)
-    year = models.IntegerField(blank=True)
-    color = models.CharField(max_length = 15, blank=True)
-    plates = models.CharField(max_length = 10, blank=True)
-    uninsured = models.BooleanField(default=False)
-    road_assistance = models.CharField(max_length = 30, blank=True)
-    accomodations = models.CharField(max_length = 30, blank=True)
 
 class Ride(models.Model):
     car = models.ForeignKey(Vehicle)
