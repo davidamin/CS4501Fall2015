6de98cc956c26cd18a5eaaed6a8f42f98c94efb4
davidamin <amin.david.m@gmail.com>
2015-09-19 15:01:44 +0000

    Added to urls

--- h1/first/urls.py
+++ h1/first/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
 	url(r'^$',views.index),
 	url(r'^add_vehicle$',views.add_vehicle),
 	url(r'^all_cars$',views.car_list),
-	url(r'^get_car/(\d+)$',views.get_car)
+	url(r'^get_car/(\d+)$',views.get_car),
+	url(r'^update_car/(\d+)$',views.update_car),
 )
