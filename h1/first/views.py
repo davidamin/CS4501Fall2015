08190fc636ec871e8e760c76f07123cafaa9962f
david <amin.david.m@gmail.com>
2015-09-19 10:53:28 -0400

    Update vehicle

--- h1/first/views.py
+++ h1/first/views.py
@@ -52,6 +52,40 @@ def get_car(request, car):
     ret_val = serializers.serialize('json',[v,])
     return JsonResponse({'ok':True,'car':ret_val})
 
+def update_car(request, car):
+    if request.method != 'POST':
+        return JsonResponse({'ok': False, 'error': 'Wrong request type, should be POST'})
+    
+    try:
+        this_car = models.Vehicle.objects.get(pk=car)
+    except:
+        return JsonResponse({'ok': False, 'error': 'Failed to find car id ' + car})
+
+    if 'max_seats' in request.POST:
+        this_car.max_seats = request.POST['max_seats']
+    if 'trunk_space' in request.POST:
+        this_car.trunk_space = request.POST['trunk_space']
+    if 'notes' in request.POST:
+        this_car.notes = request.POST['notes']
+    if 'make' in request.POST:
+        this_car.make = request.POST['make']
+    if 'model' in request.POST:
+        this_car.model = request.POST['model']
+    if 'year' in request.POST:
+        this_car.year = request.POST['year']
+    if 'color' in request.POST:
+        this_car.color = request.POST['color']
+    if 'plates' in request.POST:
+        this_car.plates = request.POST['plates']
+    if 'uninsured' in request.POST:
+        this_car.uninsured = request.POST['uninsured']
+    if 'road_assistance' in request.POST:
+        this_car.road_assistance = request.POST['road_assistance']
+    if 'accomodations' in request.POST:
+        this_car.accomodations = request.POST['accomodations']
+    this_car.save()
+    return JsonResponse({'ok':True, 'log': 'Updated vehicle ' + car})
+
 def create_user(request):
     if request.method != 'POST':
         return JsonResponse({'ok': False, 'error': 'Wrong request type, should be post'})
