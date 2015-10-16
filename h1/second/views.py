43bf32f54bb32dc6e1c1d3d37e8b166e101ba287
ericexiao <es2py@virginia.edu>
2015-10-16 16:59:42 -0400

    start of HP experience

--- h1/second/views.py
+++ h1/second/views.py
@@ -16,3 +16,16 @@ def ride_detail(request, ride):
     driver_pk = details['driver']
     vehicle_pk = details['car']
     return JsonResponse({'ok':True, 'driver': 'David Amin', 'vMake': 'Chevy', 'vModel': 'Trailblazer', 'leave': details['leave_time'], 'start': details['start'], 'arrive': details['arrive_time'], 'Destination': details['destination']})
+
+def home_detail(request):
+    r = requests.get('http://models-api:8000/models/ride_list')
+    ok = json.loads(r.text)['ok']
+    if(ok != True):
+        return JsonResponse({'ok':False})
+    ride = json.loads(r.text)['car']
+    details = json.loads(ride[1:-1])['fields']
+    driver_pk = details['driver']
+    vehicle_pk = details['car']
+    return JsonResponse({'ok':True, 'driver': 'David Amin', 'vMake': 'Chevy', 'vModel': 'Trailblazer', 'leave': details['leave_time'], 'start': details['start'], 'arrive': details['arrive_time'], 'Destination': details['destination']})
+
+    
