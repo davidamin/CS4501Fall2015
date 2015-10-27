a9eb599c4cc2e124da51c69b41c030e067347b90
UdayVarkhedkar <uvarkhedkar@gmail.com>
2015-10-27 19:15:42 -0400

    Vehicle Creation

---
+++ h1/first/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('first', '0002_authtable'),
+        ('first', '0002_auto_20151005_0259'),
+    ]
+
+    operations = [
+    ]
