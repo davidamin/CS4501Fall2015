bbae3f7115d4d5e4bb486bb0f2d1c3cdd6f996e5
UdayVarkhedkar <uvarkhedkar@gmail.com>
2015-10-27 22:50:55 -0400

    User Creation In-Progress

--- h1/third/forms.py
+++ h1/third/forms.py
@@ -17,3 +17,17 @@ class CreateVehicle(forms.Form):
 	road_assistance = forms.CharField(max_length=30, label = "Roadside-Assistance Coverage")
 	uninsured = forms.BooleanField(label = "Insured Driver", required=False)
 	notes = forms.CharField(max_length = 500, label = "Notes")
+
+class CreateUser(forms.Form):
+	username = forms.CharField(max_length=24, label="Username")
+	password = forms.CharField(widget = forms.PasswordInput())
+	first = forms.CharField(max_length=20, label="First Name")
+	last = forms.CharField(max_length=20, label="Last Name")
+	license_number = forms.CharField(max_length=20, label="License #")
+	age = forms.IntegerField(label="Age")
+	gender = forms.BooleanField(label="Gender (True if Male)")
+	email = forms.CharField(max_length=50, label="Email")
+	phone_number = forms.CharField(max_length=10, label="Phone #")
+	city = forms.CharField(max_length=100, label = "City")
+	state = forms.CharField(max_length=2, label="State")
+	payment_type = forms.IntegerField(label="Payment Type")
